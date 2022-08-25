fe701701af77e201f14b9490999dc5f214943d54
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-08-25 21:20:35 +0530

    Login Successfully

--- routes/login.js
+++ routes/login.js
@@ -3,28 +3,31 @@ const app = express()
 const router = express.Router()
 
 const Login = require('../models/login.models')
+const User = require('../models/user.models')
 
 app.use(express.json())
 
 router.post('/', async (req, res) => {
-    try{
-        const email = req.body.email;
-        const password = req.body.password;
-        const login = await Login.find();
-    
-        for (const i of login) {
-            if (email == i.email && password === i.password) {
-                //res.json(i);
-                res.send("Login Successfully.....!");
-    
-            } else {
-                res.send("Wrong Email OR password.....!");
+    try {
+        const userCount=await User.findOne({email: new RegExp('^'+req.body.email+'$', "i")}).count();
+        if (userCount!== 0){
+            const user=await User.findOne({email: new RegExp('^'+req.body.email+'$', "i")});
+            if (await user.password===req.body.password){
+                const login=new Login({
+                    email:req.body.email,
+                    password : req.body.password,
+                });
+                const response=await login.save()
+                res.send(user.firstName +" logged in")
+            }else {
+                res.send("Wrong Password.Try again")
             }
+        }else {
+            res.send("User not exist please check email")
         }
-    }catch(err){
-        res.send(err)
+    }catch (err){
+        res.send('Err'+err)
     }
-    
 })
 
 module.exports = router
